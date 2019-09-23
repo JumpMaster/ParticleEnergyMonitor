3ef230c0a3e52cd827f88874453456f60ac6df09
Kevin Cooper <kevin@cooper.uk.com>
2019-09-23 18:43:20 +0100

    Use bme280 in i2c mode and remove unused variales/classes

--- src/main.h
+++ src/main.h
@@ -2,11 +2,8 @@
 #define Main_h
 
 //#define LOGGING
-// #define DHTTYPE DHT22     // Sensor type DHT11/21/22/AM2301/AM2302
-// #define DHTPIN  D1        // Digital pin for communications
-#define BME_CS A2
 #define UNSET   -100      // A variable that wont be used by power, temperature (i hope), or humidity
-#define ENVIORNMENT_INTERVAL  30000UL // 30 seconds
+#define ENVIORNMENT_INTERVAL  60000UL // 60 seconds
 #define PUSH_RESULTS_INTERVAL 10000UL
 #define LIGHTSENSORPIN  A0
 
@@ -22,25 +19,5 @@
 
 // If Content-Length isn't given this is used for the body length increments
 const int kFallbackContentLength = 100;
-
-typedef struct
-{
-  String hostname;
-  IPAddress ip;
-  char path[200];
-  int port;
-  char body[200];
-} http_request_t;
-
-/**
- * HTTP Response struct.
- * status  response status code.
- * body    response body
- */
-typedef struct
-{
-  int status;
-  char body[200];
-} http_response_t;
 
 #endif
--- src/particleenergymonitor.cpp
+++ src/particleenergymonitor.cpp
@@ -1,24 +1,20 @@
 #include "papertrail.h"
 #include "mqtt.h"
-#include "publishqueue.h"
 #include "application.h"
-#include "HttpClient.h"
 #include "main.h"
-#include "ArduinoJson.h"
-#include "SparkFunBME280.h"
+#include "Adafruit_BME280.h"
+#include "Adafruit_Sensor.h"
 #include "secrets.h"
 
 //  Stubs
 void mqttCallback(char* topic, byte* payload, unsigned int length);
 
-float temperature = UNSET;
-float humidity = UNSET;
-float pressure = UNSET;
-int intHumidity;
-int intTemperature;
+double temperature = UNSET;
+int humidity = UNSET;
+int pressure = UNSET;
+
 volatile int power = UNSET;
-// volatile float avg_power = UNSET;
-// volatile int power_count = 0;
+bool bmePresent = false;
 
 volatile unsigned long lastWattUsedTime = 0;
 
@@ -29,17 +25,18 @@ unsigned long resetTime = 0;
 MQTT mqttClient(mqttServer, 1883, mqttCallback);
 unsigned long lastMqttConnectAttempt;
 const int mqttConnectAtemptTimeout = 5000;
+retained uint32_t lastHardResetTime;
+retained int resetCount;
 
-HttpClient http;
+Adafruit_BME280 bme; // I2C
 
-http_response_t response;
-http_request_t request;
-
-BME280 bme;
-PublishQueue pq;
-// PresenceManager pm;
-
-PapertrailLogHandler papertrailHandler(papertrailAddress, papertrailPort, "Energy-Monitor");
+PapertrailLogHandler papertrailHandler(papertrailAddress, papertrailPort,
+  "Energy-Monitor", System.deviceID(),
+  LOG_LEVEL_NONE, {
+  { "app", LOG_LEVEL_ALL }
+  // TOO MUCH!!! { “system”, LOG_LEVEL_ALL },
+  // TOO MUCH!!! { “comm”, LOG_LEVEL_ALL }
+});
 
 ApplicationWatchdog wd(60000, System.reset);
 
@@ -56,99 +53,8 @@ void lightSensorISR() {
   lastWattUsedTime = currenttime;
 }
 
-void doGetRequest() {
-  int err = 0;
-  response.status = 0;
-  response.body[0] = '\0';
-//  unsigned long httpStartTime = millis();
+void sendData(float power, double temperature, int humidity, int pressure) {
 
-  // Using IP
-  err = http.get(request.ip, request.hostname, request.path);
-
-  if (err == 0)
-  {
-    err = http.responseStatusCode();
-    if (err >= 0)
-    {
-      response.status = err;
-
-      if (err >= 200 || err < 300)
-      {
-        err = http.skipResponseHeaders();
-        if (err >= 0)
-        {
-          int bodyLen = http.contentLength();
-
-          unsigned long timeoutStart = millis();
-          if (bodyLen <= 0)
-            bodyLen = kFallbackContentLength;
-
-          char *body = (char *) malloc( sizeof(char) * ( bodyLen + 1 ) );
-
-          char c;
-          int i = 0;
-
-          while ( (http.connected() || http.available()) &&
-                 ((millis() - timeoutStart) < http.kHttpResponseTimeout))
-          {
-            // Let's make sure this character will fit into our char array
-            if (i == bodyLen)
-            {
-              // No it won't fit. Let's try and resize our body char array
-              char *temp = (char *) realloc(body, sizeof(char) * (bodyLen+kFallbackContentLength));
-
-              if ( temp != NULL ) //resize was successful
-              {
-                bodyLen += kFallbackContentLength;
-                body = temp;
-              }
-              else //there was an error
-              {
-                free(temp);
-                break;
-              }
-            }
-
-            if (http.available())
-            {
-              c = http.read();
-
-              body[i] = c;
-              i++;
-              // We read something, reset the timeout counter
-              timeoutStart = millis();
-            }
-            else
-            {
-              // We haven't got any data, so let's pause to allow some to
-              // arrive
-              delay(http.kHttpWaitForDataDelay);
-            }
-          }
-          body[i] = '\0';
-          //return body;
-          strncpy(response.body, body, bodyLen > sizeof(body) ? sizeof(body) : bodyLen);
-          //response.body = body;
-          
-          free(body);
-        }
-      }
-    }
-  }
-  http.stop();
-
-//  Serial.println();
-//  Serial.print("http request took : ");
-//  Serial.print(millis()-httpStartTime);
-//  Serial.println("ms");
-}
-
-void sendData(float power, float temperature, float humidity, float pressure) {
-  //request.path = String::format("/input/post.json?apikey=%s&node=1&json={power:%.0f,temperature:%.1f,humidity:%.1f,pressure:%.1f}", API_KEY, power, temperature, humidity, pressure);
-  
-  //char data[256];
-//   snprintf(request.path, sizeof(request.path), "/input/post.json?apikey=%s&node=1&json={power:%.0f,temperature:%.1f,humidity:%.1f,pressure:%.1f}", API_KEY, power, temperature, humidity, pressure);
-  
     if (mqttClient.isConnected()) {
         char charPower[5];
         char charTemperature[6];
@@ -156,17 +62,14 @@ void sendData(float power, float temperature, float humidity, float pressure) {
         char charPressure[7];
         snprintf(charPower, sizeof(charPower), "%.0f", power);
         snprintf(charTemperature, sizeof(charTemperature), "%.1f", temperature);
-        snprintf(charHumidity, sizeof(charHumidity), "%.0f", humidity);
-        snprintf(charPressure, sizeof(charPressure), "%.0f", pressure);
+        snprintf(charHumidity, sizeof(charHumidity), "%d", humidity);
+        snprintf(charPressure, sizeof(charPressure), "%d", pressure);
         
         mqttClient.publish("emon/particle/power", charPower, true);
         mqttClient.publish("emon/particle/temperature", charTemperature, true);
         mqttClient.publish("emon/particle/humidity", charHumidity, true);
         mqttClient.publish("emon/particle/pressure", charPressure, true);
     }
-  
-  LOGGING_PRINTLN(request.path);
-//   doGetRequest();
 }
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
@@ -178,10 +81,8 @@ void random_seed_from_cloud(unsigned seed) {
 
 STARTUP(WiFi.selectAntenna(ANT_EXTERNAL));
 STARTUP(System.enableFeature(FEATURE_RESET_INFO));
-
-int roundFloat(float x) {
-    return ((x)>=0?(int)((x)+0.5):(int)((x)-0.5));
-}
+STARTUP(System.enableFeature(FEATURE_RETAINED_MEMORY));
+SYSTEM_THREAD(ENABLED);
 
 void connectToMQTT() {
     lastMqttConnectAttempt = millis();
@@ -193,7 +94,6 @@ void connectToMQTT() {
 }
 
 void setup() {
-  WiFi.useDynamicIP();
 
   #ifdef LOGGING
     Serial.begin(9600);
@@ -204,74 +104,52 @@ void setup() {
   #endif
 
   pinMode(LIGHTSENSORPIN, INPUT);
-
-  request.ip = EMONCMS_IP;
-  request.hostname = EMONCMS_URL;
   
-  waitUntil(Particle.connected);
+  waitFor(Particle.connected, 30000);
+  
+  if (!Particle.connected)
+      System.reset();
+  do {
+      resetTime = Time.now();
+      delay(10);
+  } while (resetTime < 1000000UL && millis() < 20000);
 
-  do
-  {
-    resetTime = Time.now();
-    delay(10);
-  } while (resetTime < 1000000 && millis() < 20000);
+  if (System.resetReason() == RESET_REASON_PANIC) {
+      if ((Time.now() - lastHardResetTime) < 120) {
+          resetCount++;
+      } else {
+          resetCount = 1;
+      }
+
+      lastHardResetTime = Time.now();
+
+      if (resetCount > 3) {
+          System.enterSafeMode();
+      }
+  } else {
+      resetCount = 0;
+  }
 
   Particle.variable("resetTime", &resetTime, INT);
-  Particle.variable("humidity", intHumidity);
-  Particle.variable("temperature", intTemperature);
-  
-    // bme.reset();
-    // delay(10);
-    bme.settings.commInterface = SPI_MODE;
-	bme.settings.chipSelectPin = BME_CS;
-	//runMode can be:
-	//  0, Sleep mode
-	//  1 or 2, Forced mode
-	//  3, Normal mode
-	bme.settings.runMode = 3; //Normal mode
 
-	//tStandby can be:
-	//  0, 0.5ms
-	//  1, 62.5ms
-	//  2, 125ms
-	//  3, 250ms
-	//  4, 500ms
-	//  5, 1000ms
-	//  6, 10ms
-	//  7, 20ms
-	bme.settings.tStandby = 5;
+  unsigned bmePresent = bme.begin();
+  if (!bmePresent) {
+    Log.info("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+    Log.info("SensorID was: 0x%X", bme.sensorID());
+  } else {
+    Log.info("Valid BME280 sensor found");
 
-	//filter can be off or number of FIR coefficients to use:
-	//  0, filter off
-	//  1, coefficients = 2
-	//  2, coefficients = 4
-	//  3, coefficients = 8
-	//  4, coefficients = 16
-	bme.settings.filter = 4;
-
-	//tempOverSample can be:
-	//  0, skipped
-	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-	bme.settings.tempOverSample = 5;
-
-	//pressOverSample can be:
-	//  0, skipped
-	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-    bme.settings.pressOverSample = 5;
-
-	//humidOverSample can be:
-	//  0, skipped
-	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-	bme.settings.humidOverSample = 5;
-
-  
-    if (!bme.begin())
-        pq.publish("pushover", "Could not find a valid BME280 sensor, check wiring!");
+    bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                    Adafruit_BME280::SAMPLING_X1, // temperature
+                    Adafruit_BME280::SAMPLING_X1, // pressure
+                    Adafruit_BME280::SAMPLING_X1, // humidity
+                    Adafruit_BME280::FILTER_OFF);
+  }
   
   attachInterrupt(LIGHTSENSORPIN, lightSensorISR, FALLING);
   uint32_t resetReasonData = System.resetReasonData();
   connectToMQTT();
-  pq.publish("pushover", String::format("EnergyMonitor: Boot complete: %d-%d", System.resetReason(), resetReasonData).c_str());
+  Particle.publish("pushover", String::format("EnergyMonitor: Boot complete: %d-%d", System.resetReason(), resetReasonData).c_str(), PRIVATE);
 }
 
 void loop() {
@@ -284,15 +162,14 @@ void loop() {
         }
     }
 
-    if (currenttime > nextEnviornmentReading) {
-        temperature = bme.readTempC();
-        humidity = bme.readFloatHumidity();
-        pressure = bme.readFloatPressure() / 100.0F;
-        if (intHumidity != roundFloat(humidity) || intTemperature != roundFloat(temperature)) {
-            intHumidity = roundFloat(humidity);
-            intTemperature = roundFloat(temperature);
-        }
+    if (bmePresent && currenttime > nextEnviornmentReading) {
         nextEnviornmentReading = currenttime + ENVIORNMENT_INTERVAL;
+
+        bme.takeForcedMeasurement();
+
+        temperature = round(bme.readTemperature()*2.0) / 2.0;
+        humidity = (int) round(bme.readHumidity());
+        pressure = (int) round(bme.readPressure() / 100.0F);
     }
 
     if (mqttClient.isConnected()) {
@@ -302,6 +179,5 @@ void loop() {
         connectToMQTT();
     }
 
-    pq.process();
     wd.checkin(); // resets the AWDT count
 }
